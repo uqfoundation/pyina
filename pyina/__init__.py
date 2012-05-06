46b013031d8f891b9dd0867c23fd0a06112551e7
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2012-05-06 17:56:00 +0000

    enable better versioning in setup.py __init__.py; add setup.cfg; autogenerate README and XXX.info (with versioned information)
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/pyina@464 8bfda07e-5b16-0410-ab1d-fd04ec2748df

--- pyina/__init__.py
+++ pyina/__init__.py
@@ -3,193 +3,27 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 #                              Mike McKerns, Caltech
-#                        (C) 1997-2011  All Rights Reserved
+#                        (C) 1997-2012  All Rights Reserved
 #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+# get version numbers, license, and long description
+try:
+    from info import this_version as __version__
+    from info import readme as __doc__, license as __license__
+except ImportError:
+    msg = """First run 'python setup.py build' to build pyina."""
+    raise ImportError(msg)
 
-"""
-pyina: a MPI-based parallel mapper and launcher
-
-The pyina package provides several basic tools to make MPI-based
-high-performance computing more accessable to the end user. The goal
-of pyina is to allow the user to extend their own code to MPI-based
-high-performance computing with minimal refactoring.
-
-The central element of pyina is the parallel map-reduce algorithm.
-Pyina currently provides two strategies for executing the parallel-map,
-where a strategy is the algorithm for distributing the work list of
-jobs across the availble nodes.  These strategies can be used "in-the-raw"
-(i.e. directly) to provide map-reduce to a user's own mpi-aware code.
-Further, pyina provides the "ez_map" interface, which is a map-reduce
-implementation that hides the MPI internals from the user. With ez_map,
-the user can launch their code in parallel batch mode -- using standard
-python and without ever having to write a line of parallel python or MPI code.
-
-There are several ways that a user would typically launch their code in
-parallel -- directly with "mpirun" or "mpiexec", or through the use of a
-scheduler such as torque or slurm. Pyina encapsulates several of these
-'launchers', and provides a common interface to the different methods of
-launching a MPI job.
-
-Pyina is part of pathos, a python framework for heterogenous computing.
-Pyina is in the early development stages, and any user feedback is
-highly appreciated. Contact Mike McKerns [mmckerns at caltech dot edu]
-with comments, suggestions, and any bugs you may find. A list of known
-issues is maintained at http://dev.danse.us/trac/pathos/query.
-
-
-Major Features
-==============
-
-Pyina provides a highly configurable parallel map-reduce interface
-to running MPI jobs, with::
-    - a map-reduce interface that extends the python 'map' standard
-    - the ability to submit batch jobs to a selection of schedulers
-    - the ability to customize node and process launch configurations
-    - the ability to launch parallel MPI jobs with standard python
-    - ease in selecting different strategies for processing a work list
-
-
-Current Release
-===============
-
-This release version is pyina-0.1a2.dev. You can download it here.
-The latest version of pyina is available from::
-    http://dev.danse.us/trac/pathos
-
-Pyina is distributed under a modified BSD license.
-
-
-Installation
-============
-
-Pyina is packaged to install from source, so you must
-download the tarball, unzip, and run the installer::
-    [download]
-    $ tar -xvzf pyina-0.1a2.dev.tgz
-    $ cd pyina-0.1a2.dev
-    $ python setup py build
-    $ python setup py install
-
-You will be warned of any missing dependencies and/or settings after
-you run the "build" step above. Pyina depends on dill and mpi4py,
-so you should install them first. A version of MPI must also be
-installed. Pyina's launchers that submit to a scheduler will throw errors
-if the underlying scheduler is not available, however a scheduler is not
-required for pyina to execute.
-
-Alternately, pyina can be installed with easy_install::
-    [download]
-    $ easy_install -f . pyina
-
-
-Requirements
-============
-
-Pyina requires::
-    - python, version >= 2.5, version < 3.0
-    - mpi4py, version >= 1.2.1
-    - dill, version >= 0.1a1
-
-Optional requirements::
-    - setuptools, version >= 0.6
-    - pyre, version == 0.8
-    - mystic, version >= 0.2a1
-
-
-Usage Notes
-===========
-
-Probably the best way to get started is to look at a few of the
-examples provided within pyina. See `pyina.examples` for a
-set of scripts that demonstrate the configuration and launching of
-mpi-based parallel jobs using the `ez_map` interface. Also see
-`pyina.examples_other` for a set of scripts that test the more raw
-internals of pyina.
-
-Important classes and functions are found here::
-    - pyina.pyina.ez_map        [the map-reduce API definition]
-    - pyina.pyina.mappers       [all available strategies] 
-    - pyina.pyina.schedulers    [all available schedulers] 
-    - pyina.pyina.launchers     [all available launchers] 
-
-Mapping strategies are found here::
-    - pyina.pyina.parallel_map  [the card-dealer strategy]
-    - pyina.pyina.parallel_map2 [the equal-portion strategy]
-
-Pyina also provides two convience scripts that help navigate the
-MPI environment. These scripts are installed to a directory on the
-user's $PATH, and thus can be run from anywhere::
-    - machines.py               [list the available MPI nodes]
-    - mpi_world.py              [setup/teardown of the MPI environment]
-
-If may also be convienent to set a shell alias for the launch of 'raw'
-mpi-python jobs. Set something like the following (for bash)::
-    $ alias mpython1='mpiexec -np 1 `which python`'
-    $ alias mpython2='mpiexec -np 2 `which python`'
-    $ ...
-
-
-More Information
-================
-
-Please see http://dev.danse.us/trac/pathos/pyina for further information.
-"""
-__version__ = '0.1a2.dev'
 __author__ = 'Mike McKerns'
 
+__doc__ = """
+""" + __doc__
+
 __license__ = """
-This software is part of the open-source DANSE project at the California
-Institute of Technology, and is available subject to the conditions and
-terms laid out below. By downloading and using this software you are
-agreeing to the following conditions.
+""" + __license__
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions
-are met::
-
-    - Redistribution of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-
-    - Redistribution in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentations and/or other materials provided with the distribution.
-
-    - Neither the name of the California Institute of Technology nor
-      the names of its contributors may be used to endorse or promote
-      products derived from this software without specific prior written
-      permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-Copyright (c) 2011 California Institute of Technology. All rights reserved.
-
-
-If you use this software to do productive scientific research that leads to
-publication, we ask that you acknowledge use of the software by citing the
-following in your publication::
-
-    M.M. McKerns, L. Strand, T. Sullivan, A. Fang, M.A.G. Aivazis,
-    "Building a framework for predictive science", Proceedings of
-    the 10th Python in Science Conference, (submitted 2011).
-
-    Michael McKerns and Michael Aivazis,
-    "pathos: a framework for heterogeneous computing", 2010- ;
-    http://dev.danse.us/trac/pathos
-
-"""
 # shortcuts
 from mpi4py import MPI as mpi
 mpi.world = mpi.COMM_WORLD
@@ -210,9 +44,14 @@ import parallel_map, parallel_map2
 # tools
 from tools import *
 
-def copyright():
-    """print copyright and reference"""
-    print __license__[-599:]
+def license():
+    """print license"""
+    print __license__
+    return
+
+def citation():
+    """print citation"""
+    print __doc__[-499:-140]
     return
 
 # end of file
--- setup.py
+++ setup.py
@@ -3,6 +3,14 @@
 # Michael McKerns
 # mmckerns@caltech.edu
 
+from __future__ import with_statement
+import os
+
+# set version numbers
+stable_version = '0.1a1'
+target_version = '0.2a'
+is_release = False
+
 # check if easy_install is available
 try:
 #   import __force_distutils__ #XXX: uncomment to force use of distutills
@@ -11,6 +19,229 @@ try:
 except ImportError:
     from distutils.core import setup
     has_setuptools = False
+
+# generate version number
+if os.path.exists('pyina/info.py'):
+    # is a source distribution, so use existing version
+    from pyina.info import this_version
+elif stable_version == target_version:
+    # we are building a stable release
+    this_version = target_version
+else:
+    # we are building a distribution
+    this_version = target_version + '.dev'
+    if is_release:
+      from datetime import date
+      today = "".join(date.isoformat(date.today()).split('-'))
+      this_version += "-" + today
+
+# get the license info
+with open('LICENSE') as file:
+    license_text = file.read()
+
+# generate the readme text
+long_description = \
+"""-----------------------------------------------
+pyina: a MPI-based parallel mapper and launcher
+-----------------------------------------------
+
+The pyina package provides several basic tools to make MPI-based
+high-performance computing more accessable to the end user. The goal
+of pyina is to allow the user to extend their own code to MPI-based
+high-performance computing with minimal refactoring.
+
+The central element of pyina is the parallel map-reduce algorithm.
+Pyina currently provides two strategies for executing the parallel-map,
+where a strategy is the algorithm for distributing the work list of
+jobs across the availble nodes.  These strategies can be used "in-the-raw"
+(i.e. directly) to provide map-reduce to a user's own mpi-aware code.
+Further, pyina provides the "ez_map" interface, which is a map-reduce
+implementation that hides the MPI internals from the user. With ez_map,
+the user can launch their code in parallel batch mode -- using standard
+python and without ever having to write a line of parallel python or MPI code.
+
+There are several ways that a user would typically launch their code in
+parallel -- directly with "mpirun" or "mpiexec", or through the use of a
+scheduler such as torque or slurm. Pyina encapsulates several of these
+'launchers', and provides a common interface to the different methods of
+launching a MPI job.
+
+Pyina is part of pathos, a python framework for heterogenous computing.
+Pyina is in the early development stages, and any user feedback is
+highly appreciated. Contact Mike McKerns [mmckerns at caltech dot edu]
+with comments, suggestions, and any bugs you may find. A list of known
+issues is maintained at http://dev.danse.us/trac/pathos/query.
+
+
+Major Features
+==============
+
+Pyina provides a highly configurable parallel map-reduce interface
+to running MPI jobs, with::
+
+    - a map-reduce interface that extends the python 'map' standard
+    - the ability to submit batch jobs to a selection of schedulers
+    - the ability to customize node and process launch configurations
+    - the ability to launch parallel MPI jobs with standard python
+    - ease in selecting different strategies for processing a work list
+
+
+Current Release
+===============
+
+The latest stable release version is pyina-%(relver)s. You can download it here.
+The latest stable version of pyina is always available at:
+
+    http://dev.danse.us/trac/pathos
+
+
+Development Release
+===================
+
+If you like living on the edge, and don't mind the promise
+of a little instability, you can get the latest development
+release with all the shiny new features at:
+
+    http://dev.danse.us/packages.
+
+
+Installation
+============
+
+Pyina is packaged to install from source, so you must
+download the tarball, unzip, and run the installer::
+
+    [download]
+    $ tar -xvzf pyina-%(thisver)s.tgz
+    $ cd pyina-%(thisver)s
+    $ python setup py build
+    $ python setup py install
+
+You will be warned of any missing dependencies and/or settings after
+you run the "build" step above. Pyina depends on dill and mpi4py,
+so you should install them first. A version of MPI must also be
+installed. Pyina's launchers that submit to a scheduler will throw errors
+if the underlying scheduler is not available, however a scheduler is not
+required for pyina to execute.
+
+The current implementation of pyina required slight modifications
+to one external dependency. This dependency and installation
+instructions are provided in the `pyina.external` directory for users
+installing without setuptools.
+
+Alternately, pyina can be installed with easy_install::
+
+    [download]
+    $ easy_install -f . pyina
+
+
+Requirements
+============
+
+Pyina requires::
+
+    - python, version >= 2.5, version < 3.0
+    - mpi4py, version >= 1.2.1
+    - dill, version >= 0.2a.dev
+
+Optional requirements::
+
+    - setuptools, version >= 0.6
+    - pyre, version == 0.8
+    - mystic, version >= 0.2a2.dev
+
+
+Usage Notes
+===========
+
+Probably the best way to get started is to look at a few of the
+examples provided within pyina. See `pyina.examples` for a
+set of scripts that demonstrate the configuration and launching of
+mpi-based parallel jobs using the `ez_map` interface. Also see
+`pyina.examples_other` for a set of scripts that test the more raw
+internals of pyina.
+
+Important classes and functions are found here::
+
+    - pyina.pyina.ez_map        [the map-reduce API definition]
+    - pyina.pyina.mappers       [all available strategies] 
+    - pyina.pyina.schedulers    [all available schedulers] 
+    - pyina.pyina.launchers     [all available launchers] 
+
+Mapping strategies are found here::
+
+    - pyina.pyina.parallel_map  [the card-dealer strategy]
+    - pyina.pyina.parallel_map2 [the equal-portion strategy]
+
+Pyina also provides two convience scripts that help navigate the
+MPI environment. These scripts are installed to a directory on the
+user's $PATH, and thus can be run from anywhere::
+
+    - machines.py               [list the available MPI nodes]
+    - mpi_world.py              [setup/teardown of the MPI environment]
+
+If may also be convienent to set a shell alias for the launch of 'raw'
+mpi-python jobs. Set something like the following (for bash)::
+
+    $ alias mpython1='mpiexec -np 1 `which python`'
+    $ alias mpython2='mpiexec -np 2 `which python`'
+    $ ...
+
+
+License
+=======
+
+Pyina is distributed under a modified BSD license.
+
+    >>> import pyina
+    >>> print pyina.license()
+
+
+Citation
+========
+
+If you use pyina to do research that leads to publication,
+we ask that you acknowledge use of pyina by citing the
+following in your publication::
+
+    M.M. McKerns, L. Strand, T. Sullivan, A. Fang, M.A.G. Aivazis,
+    "Building a framework for predictive science", Proceedings of
+    the 10th Python in Science Conference, 2011;
+    http://arxiv.org/pdf/1202.1056
+
+    Michael McKerns and Michael Aivazis,
+    "pathos: a framework for heterogeneous computing", 2010- ;
+    http://dev.danse.us/trac/pathos
+
+
+More Information
+================
+
+Please see http://dev.danse.us/trac/pathos or http://arxiv.org/pdf/1202.1056 for further information.
+
+""" % {'relver' : stable_version, 'thisver' : this_version}
+
+# write readme file
+with open('README', 'w') as file:
+    file.write(long_description)
+
+# generate 'info' file contents
+def write_info_py(filename='pyina/info.py'):
+    contents = """# THIS FILE GENERATED FROM SETUP.PY
+this_version = '%(this_version)s'
+stable_version = '%(stable_version)s'
+readme = '''%(long_description)s'''
+license = '''%(license_text)s'''
+"""
+    with open(filename, 'w') as file:
+        file.write(contents % {'this_version' : this_version,
+                               'stable_version' : stable_version,
+                               'long_description' : long_description,
+                               'license_text' : license_text })
+    return
+
+# write info file
+write_info_py()
 
 # platform-specific instructions
 sdkroot_set = False
@@ -35,12 +266,13 @@ else: #platform = linux or mac
 # build the 'setup' call
 setup_code = """
 setup(name="pyina",
-    version="0.1a2.dev",
+    version='%s',
     maintainer="Mike McKerns",
     maintainer_email="mmckerns@caltech.edu",
     license="BSD",
     platforms=["any"],
     description="a MPI-based parallel mapper and launcher",
+    long_description = '''%s''',
     classifiers=(
         "Intended Audience :: Developers",
         "Programming Language :: Python",
@@ -49,15 +281,15 @@ setup(name="pyina",
 
     packages=['pyina'],
     package_dir={'pyina':'pyina'},
-"""
+""" % (target_version, long_description)
 
 # add dependencies
-dill_version = '>=0.1a1'
+dill_version = '>=0.2a.dev'
 mpi4py_version = '>=1.2.1'
 if platform[:6] == 'darwin':
   mpi4py_version = '>=1.2.2-pyina'
 pypar_version = '>=2.1.4'
-mystic_version = '>=0.2a1'
+mystic_version = '>=0.2a2.dev'
 if has_setuptools:
     setup_code += """
         zip_safe = False,
