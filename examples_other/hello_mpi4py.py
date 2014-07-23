cfbc0c3d1fc70379aaaeefdbcc285f76d271268c
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2014-07-23 04:31:31 +0000

    added hello_mpi4py.py to check mpi4py installation
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/pyina@683 8bfda07e-5b16-0410-ab1d-fd04ec2748df

---
+++ examples_other/hello_mpi4py.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+#
+# source code modifed from mpi4py-1.3.1/demo/helloworld.py
+# 
+# Copyright (c) 2013, Lisandro Dalcin.
+# All rights reserved.
+"""
+# Parallel Hello World, with mpi4py
+# To run:
+
+alias mpython='mpirun -np [#nodes] `which python`'
+mpython hello_mpi4py.py
+"""
+
+from mpi4py import MPI
+import sys
+
+size = MPI.COMM_WORLD.Get_size()
+rank = MPI.COMM_WORLD.Get_rank()
+name = MPI.Get_processor_name()
+
+sys.stdout.write(
+    "Hello, World! I am process %d of %d on %s.\n"
+    % (rank, size, name))
+
+
+
+
+"""
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+# EOF
