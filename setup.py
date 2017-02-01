0aff52e7e164bcfbef7c8466a3398cd7a6efdac9
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2017-02-01 19:45:44 +0000

    nudging past dill-0.2.7
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/pyina@958 8bfda07e-5b16-0410-ab1d-fd04ec2748df

--- setup.py
+++ setup.py
@@ -154,7 +154,7 @@ Requirements
     - python, version >= 2.5, version < 3.0
     - numpy, version >= 1.0
     - mpi4py, version >= 1.2.1
-    - dill, version >= 0.2.5
+    - dill, version >= 0.2.6
     - pox, version >= 0.2.3
     - pathos, version >= 0.2.0
 
@@ -300,7 +300,7 @@ setup(name="pyina",
 
 # add dependencies
 numpy_version = '>=1.0'
-dill_version = '>=0.2.5'
+dill_version = '>=0.2.6'
 pox_version = '>=0.2.3'
 pathos_version = '>=0.2.0'
 mpi4py_version = '>=1.2.1'
