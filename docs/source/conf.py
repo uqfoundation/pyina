aae51a956a6a91a16b79170652a55dda1dc4f297
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2018-04-13 21:51:42 +0000

    try mock of mpi4py for rtd
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/pyina@1033 8bfda07e-5b16-0410-ab1d-fd04ec2748df

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,18 @@ sys.path.insert(0, os.path.abspath('../../scripts'))
 # Import the project
 import pyina
 
+# Mock mpi4py
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['mpi4py']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
