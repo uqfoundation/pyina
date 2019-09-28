88a6ac8fec6c711774d6d9917da127814ac3e3cf
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2019-09-28 18:45:54 +0000

    bumping past mystic-0.3.5
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/pyina@1259 8bfda07e-5b16-0410-ab1d-fd04ec2748df

--- setup.py
+++ setup.py
@@ -159,7 +159,7 @@ Requirements
 Optional requirements:
 
     - ``setuptools``, **version >= 0.6**
-    - ``mystic``, **version >= 0.3.4**
+    - ``mystic``, **version >= 0.3.5**
 
 
 More Information
@@ -296,7 +296,7 @@ pox_version = '>=0.2.7'
 pathos_version = '>=0.2.5'
 mpi4py_version = '>=1.3'
 pypar_version = '>=2.1.4'
-mystic_version = '>=0.3.4'
+mystic_version = '>=0.3.5'
 # rtd fails for mpi4py, mock it instead
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if has_setuptools:
