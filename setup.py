9673efd51f8a808b946e96a6cf6137e487b24523
mmckerns <mmckerns@caltech.edu>
2021-06-13 13:02:42 -0400

    bumping to pox-0.3.0

--- setup.py
+++ setup.py
@@ -160,7 +160,7 @@ Requirements
     - ``numpy``, **version >= 1.0**
     - ``mpi4py``, **version >= 1.3**
     - ``dill``, **version >= 0.3.3**
-    - ``pox``, **version >= 0.2.9**
+    - ``pox``, **version >= 0.3.0**
     - ``pathos``, **version >= 0.2.7**
 
 Optional requirements:
@@ -321,7 +321,7 @@ elif sysversion == (2,7) or sysversion == (3,4):
 else:
     numpy_version = '>=1.0'
 dill_version = '>=0.3.3'
-pox_version = '>=0.2.9'
+pox_version = '>=0.3.0'
 pathos_version = '>=0.2.7'
 mpi4py_version = '>=1.3, !=3.0.2' # segfault 11 on MPI import
 pypar_version = '>=2.1.4'
