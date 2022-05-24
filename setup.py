f88efcf147f18c6ec3e52149800cdd894128b371
mmckerns <mmckerns@caltech.edu>
2022-05-24 11:43:15 -0400

    bumping past mystic-0.3.9

--- setup.py
+++ setup.py
@@ -137,7 +137,7 @@ sysversion = sys.version_info[:2]
 dill_version = 'dill>=0.3.5.1'
 pox_version = 'pox>=0.3.1'
 pathos_version = 'pathos>=0.2.9'
-mystic_version = 'mystic>=0.3.8'
+mystic_version = 'mystic>=0.3.9'
 cython_version = 'cython>=0.29.22' #XXX: required to build numpy from source
 try:
     import ctypes # if using `pypy`, pythonapi is not found
